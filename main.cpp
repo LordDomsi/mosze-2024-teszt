bdfb26545bb8cdaaacbc712808e1dd80004923d7
LordDomsi <domsi2001@gmail.com>
2024-10-07 20:54:09 +0200

    main.cpp hibainak kommentelese

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];    //N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'    //; hianyzik
+    for (int i = 0;)    //for ciklus hianyos, (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)    //for ciklus hianyos, (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"    // << b[i];  
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag;    //int helyett float
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i]    //; hianyzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
